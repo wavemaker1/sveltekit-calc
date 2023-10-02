eac4575d4f5318c049cfe7fd9491509b850e6ae5
wavemaker1 <get2no1@naver.com>
2023-10-02 18:16:55 +0900

    modify border

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -3,20 +3,20 @@
 
 	$: path = $page.url.pathname;
 	let tabs = [
-		{ name: '디자인', path: '/Design' },
-		{ name: '글자', path: '/Text' },
-		{ name: '색상/마감', path: '/FnPaint' },
-		{ name: 'LED', path: '/LED' },
-        { name: '홀더', path: '/Holder' }
+		{ name: '디자인', path: '/Design', active:'true'},
+		{ name: '글자', path: '/Text' , active:'false'},
+		{ name: '색&마감', path: '/FnPaint' , active:'false'},
+		{ name: 'LED', path: '/LED' , active:'false'},
+        { name: '홀더', path: '/Holder' , active:'false'}
 	];
 </script>
 
 <div class="flex flex-col h-full">
-	<nav class="flex md:flex-row justify-between md:justify-start md:space-x-0 space-x-2 text-[#000000]">
+	<nav class="flex md:flex-row justify-between md:justify-start md:space-x-0 text-[#000000]">
 		{#each tabs as tab, i (i)}
 			<a
 				href={tab.path}
-				class={`${ path === tab.path ? 'bg-teal-300	border-amber-50	 border-2 shadow-cyan-500 shadow-md' : 'bg-teal-50' } p-2 w-1/5  shadow-md shadow-zinc-700/30 text-center`}
+				class={`${ path === tab.path ? 'bg-teal-300	border-amber-50	 shadow-cyan-500 shadow-md' : 'bg-teal-50' } p-2 w-1/5   text-center`}
 			>
 				{tab.name}
 			</a>
